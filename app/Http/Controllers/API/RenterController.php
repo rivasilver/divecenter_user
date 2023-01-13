f33684fc07774c757b440f88705884a7ba301076
Vágó Rebeka <rebeka.vago19@gmail.com>
2023-01-13 20:16:25 +0100

    controller javítva

--- app/Http/Controllers/API/RenterController.php
+++ app/Http/Controllers/API/RenterController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Renter;
+use App\Http\Requests\StoreRenterRequest;
+use App\Http\Requests\UpdateRenterRequest;
 
 class RenterController extends Controller
 {
